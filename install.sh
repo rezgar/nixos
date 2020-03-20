2e96aeec6b120dcc4d1a1e9ce2f8be2e33cd6664
Rezgar Cadro <rezgar.cadro@gmail.com>
2020-03-20 19:59:41 +0200

    Adopting the installation script to /bin/sh (WIP)

--- install.sh
+++ install.sh
@@ -1,4 +1,18 @@
-#! /bin/bash
+#! /bin/sh
+abort()
+{
+    echo >&2 '
+***************
+*** ABORTED ***
+***************
+'
+    echo "An error occurred. Exiting..." >&2
+    exit 1
+}
+
+trap 'abort' 0
+
+set -e
 
 POSITIONAL=()
 while [[ $# -gt 0 ]]
