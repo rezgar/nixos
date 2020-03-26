fac995ad9133d88962731d5110125b72db519d31
Rezgar Cadro <rezgar.cadro@gmail.com>
2020-03-26 22:39:03 +0200

    User customization

--- user.nix
+++ user.nix
@@ -1,18 +1,14 @@
 { config, pkgs, ... }:
 
-# Use your own OS user name
 let
-    username = "user";
+    username = "rezgar";
 in {
-    # Add/modify existing default or custom imports 
     imports = [
         ./desktop/kde/nix
     ];
 
-    # Set your time zone.
     time.timeZone = "Europe/Chisinau";
     
-    # Define a user account. Don't forget to set a password with ‘passwd’.
     users.users.${username} = {
         isNormalUser = true;
             extraGroups = [
@@ -23,6 +19,6 @@ in {
     };
 
     environment.systemPackages = with pkgs; [
-        # Insert your custom packages here (separated with a space)
+        mattermos-desktop
     ];
 }
