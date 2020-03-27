33eb83230dcb5231b332081ff47b95e814157599
Rezgar Cadro <rezgar.cadro@gmail.com>
2020-03-27 10:51:57 +0200

    Default mail client removed, done per DM

--- email.nix
+++ email.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: 
 {
   environment.systemPackages = with pkgs; [
-    inboxer
+    # There's no visually appealing mail client that works on all platforms. "kmail" is good for KDE, but doesn't work with i3. It's best to use different ones per DM
   ];
 }
